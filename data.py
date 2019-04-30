3c508732fac294545fd1117a7ed6e213e68f212c
Antony Yun <antony.n.yun@gmail.com>
2019-04-30 16:41:35 -0500

    Format and fix

--- data.py
+++ data.py
@@ -3,13 +3,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from torchtext.data import Field, BucketIterator, TabularDataset
 
-NEWSELA_FILE = './newsela_article_corpus_2016-01-29/newsela_data_share-20150302/newsela_articles_20150302.aligned.sents.txt'
+NEWSELA_FILE = './newsela_article_corpus_2016-01-29/' \
+    'newsela_data_share-20150302/newsela_articles_20150302.aligned.sents.txt'
 
 newsela_df = pd.read_csv(NEWSELA_FILE, sep='\t',
                          names=['doc_id', 'doc_version_complex',
                                 'doc_version_simple', 'sentence_complex',
                                 'sentence_simple']
-                        )
+                         )
 
 newsela_df.dropna(inplace=True)
 train, val = train_test_split(newsela_df, test_size=0.1)
@@ -17,23 +18,32 @@ train, val = train_test_split(newsela_df, test_size=0.1)
 train.to_csv("train.csv", index=False)
 val.to_csv("val.csv", index=False)
 
+
 def tok(x):
-    # print(x)
-    # return map(wordpunct_tokenize, x)
     return wordpunct_tokenize(x)
+
 
 SIMPLE_TEXT = Field(tokenize=tok)
 COMPLEX_TEXT = Field(tokenize=tok)
 
-data_fields = [('doc_id', None), ('doc_version_complex', None), ('doc_version_simple', None),
-               ('sentence_complex', SIMPLE_TEXT), ('sentence_simple', COMPLEX_TEXT)]
+data_fields = [
+    ('doc_id', None),
+    ('doc_version_complex', None),
+    ('doc_version_simple', None),
+    ('sentence_complex', SIMPLE_TEXT),
+    ('sentence_simple', COMPLEX_TEXT)]
 
-train, val = TabularDataset.splits(path='./', train='train.csv', validation='val.csv',
-                                   format='csv', fields=data_fields)
+train, val = TabularDataset.splits(
+    path='./', train='train.csv', validation='val.csv', format='csv',
+    fields=data_fields)
 
 SIMPLE_TEXT.build_vocab(train, val, vectors="glove.6B.100d")
 COMPLEX_TEXT.vocab = SIMPLE_TEXT.vocab
 
-train_iter, val_iter = BucketIterator.splits((train, val), batch_size=(64, 64),
-                                             shuffle=True,
-                                             sort_key=lambda x: len(x.sentence_complex))
+train_iter, val_iter = BucketIterator.splits(
+    (train, val), batch_sizes=(
+        64, 64), shuffle=True, sort_key=lambda x: len(
+            x.sentence_complex))
+
+train_iter = iter(train_iter)
+val_iter = iter(val_iter)
